c0ecde92738538efb46e149c93eda5f7d508d38c
nlom0218 <nlom0218@naver.com>
2022-04-07 20:56:59 +0900

    환경설정2

---
+++ algorithm.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Algorithm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.CursorVisible = false;
+
+            const int WAIT_TICK = 1000 / 30;
+            const char CIRCLE = '\u25cf';
+
+            int lastTick = 0;
+
+            while (true)
+            {
+                int currentTick = System.Environment.TickCount;
+                if (currentTick - lastTick < WAIT_TICK)
+                    continue;
+                lastTick = currentTick;
+
+                Console.SetCursorPosition(0, 0);
+                for (int i = 0; i < 25; i++)
+                {
+                    for (int j = 0; j < 25; j++)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.Write(CIRCLE);
+                    }
+                    Console.WriteLine();
+                }
+            }
+        }
+    }
+}
