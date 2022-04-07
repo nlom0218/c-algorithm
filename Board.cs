5ecde91d20f2e22c1375e9f449a94e49eff59d1c
nlom0218 <nlom0218@naver.com>
2022-04-07 21:23:23 +0900

    배열, 동적 배열, 연결 리스트 비고

---
+++ Board.cs
@@ -0,0 +1,13 @@
+namespace Algorithm
+{
+    class Board
+    {
+        public int[] _data = new int[25];
+        public List<int> _data2 = new List<int>();
+        public LinkedList<int> _data3 = new LinkedList<int>();
+        public void Initialize()
+        {
+
+        }
+    }
+}
