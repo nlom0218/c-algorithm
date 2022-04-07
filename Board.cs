0b3b26792c11ae2af0ce4107663269abb0c8e599
nlom0218 <nlom0218@naver.com>
2022-04-07 22:13:00 +0900

    동적 배열 구현 연습

--- Board.cs
+++ Board.cs
@@ -1,13 +1,66 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
 namespace Algorithm
 {
+    class MyList<T>
+    {
+        const int DEFAULT_SIZE = 1;
+        T[] _data = new T[DEFAULT_SIZE];
+        public int Count = 0; // 실제로 사용중인 데이터 개수
+        public int Capacity { get { return _data.Length; } }// 예약된 데이터 개수
+
+        public void Add(T item)
+        {
+            //1. 공간이 충분히 남아 있는지 확인
+            if (Count >= Capacity)
+            {
+                // 공간을 다시 늘려서 확보
+                T[] newArray = new T[Count * 2];
+                for (int i = 0; i < Count; i++)
+                    newArray[i] = _data[i];
+                _data = newArray;
+            }
+            //2. 공간에다가 데이터를 넣어준다.
+            _data[Count] = item;
+            Count++;
+        }
+
+        public T this[int index]
+        {
+            get { return _data[index]; }
+            set { _data[index] = value; }
+        }
+
+        public void RemoveAt(int index)
+        {
+            // 101 102 103 104 105
+            for (int i = index; i < Count - 1; i++)
+            {
+                _data[i] = _data[i + 1];
+            }
+            _data[Count - 1] = default(T);
+            Count--;
+        }
+    }
     class Board
     {
-        public int[] _data = new int[25];
-        public List<int> _data2 = new List<int>();
-        public LinkedList<int> _data3 = new LinkedList<int>();
+        public int[] _data = new int[25]; // 배열
+        public MyList<int> _data2 = new MyList<int>(); // 동적 배열
+        public LinkedList<int> _data3 = new LinkedList<int>(); // 연결 리스트
         public void Initialize()
         {
 
+            _data2.Add(101);
+            _data2.Add(102);
+            _data2.Add(103);
+            _data2.Add(104);
+            _data2.Add(105);
+
+            int temp = _data2[2];
+
+            _data2.RemoveAt(2);
         }
     }
 }
