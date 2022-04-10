639d92a5810beb0faf9dc872f4c59c4f4fbc471b
nlom0218 <nlom0218@naver.com>
2022-04-10 10:46:48 +0900

    Binary Tree 미로 생성 알고리즘

--- Board.cs
+++ Board.cs
@@ -16,21 +16,64 @@ namespace Algorithm
         }
         public void Initialize(int size)
         {
+            if (size % 2 == 0)
+                return;
+
             _tile = new TileType[size, size];
             _size = size;
 
+            GenerateByBinaryTree();
+        }
+
+        void GenerateByBinaryTree()
+        {
+
+            // 일단 길을 모두 막아버리는 작업
             for (int y = 0; y < _size; y++)
             {
                 for (int x = 0; x < _size; x++)
                 {
-                    if (x == 0 || x == _size - 1 || y == 0 || y == _size - 1)
-                    {
+                    if (x % 2 == 0 || y % 2 == 0)
                         _tile[y, x] = TileType.Wall;
+                    else
+                        _tile[y, x] = TileType.Empty;
+
+                }
+            }
+
+            // 랜덤으로 우측 혹은 아래로 길을 뚫는 작업
+            Random rand = new Random();
+            for (int y = 0; y < _size; y++)
+            {
+                for (int x = 0; x < _size; x++)
+                {
+                    if (x % 2 == 0 || y % 2 == 0)
+                        continue;
+
+                    if (y == _size - 2 && x == _size - 2)
+                        continue;
+
+                    if (y == _size - 2)
+                    {
+                        _tile[y, x + 1] = TileType.Empty;
+                        continue;
+                    }
+
+                    if (x == _size - 2)
+                    {
+                        _tile[y + 1, x] = TileType.Empty;
+                        continue;
+                    }
+
+                    if (rand.Next(0, 2) == 0)
+                    {
+                        _tile[y, x + 1] = TileType.Empty;
                     }
                     else
                     {
-                        _tile[y, x] = TileType.Empty;
+                        _tile[y + 1, x] = TileType.Empty;
                     }
+
                 }
             }
         }
