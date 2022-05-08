a299996e150fdff6b5b1ed6a1092dba38149f909
nlom0218 <nlom0218@naver.com>
2022-05-08 09:48:55 +0900

    그래프 생성

--- Program.cs
+++ Program.cs
@@ -3,6 +3,28 @@ using System.Collections.Generic;
 
 namespace Algorithm
 {
+    class Graph
+    {
+        int[,] adj = new int[6, 6]
+        {
+            {0, 1, 0, 1, 0, 0},
+            {1, 0, 1, 1, 0, 0},
+            {0, 1, 0, 0, 0, 0},
+            {1, 1, 0, 0, 1, 0},
+            {0, 0, 0, 1, 0, 1},
+            {0, 0, 0, 0, 1, 0},
+        };
+
+        List<int>[] adj2 = new List<int>[]
+        {
+            new List<int>() {1, 3},
+            new List<int>() {0, 2, 3},
+            new List<int>() {1},
+            new List<int>() {0, 1, 4},
+            new List<int>() {3, 5},
+            new List<int>() {4},
+        };
+    }
     class Program
     {
         static void Main(string[] args)
@@ -40,28 +62,34 @@ namespace Algorithm
 
             // [1] [2] [3] [4]
 
-            Stack<int> stack = new Stack<int>();
-            stack.Push(101);
-            stack.Push(102);
-            stack.Push(103);
-            stack.Push(104);
-            stack.Push(105);
+            // Stack<int> stack = new Stack<int>();
+            // stack.Push(101);
+            // stack.Push(102);
+            // stack.Push(103);
+            // stack.Push(104);
+            // stack.Push(105);
 
-            int data = stack.Pop();
-            int data2 = stack.Peek();
+            // int data = stack.Pop();
+            // int data2 = stack.Peek();
 
-            Queue<int> queue = new Queue<int>();
-            queue.Enqueue(101);
-            queue.Enqueue(102);
-            queue.Enqueue(103);
-            queue.Enqueue(104);
-            queue.Enqueue(105);
+            // Queue<int> queue = new Queue<int>();
+            // queue.Enqueue(101);
+            // queue.Enqueue(102);
+            // queue.Enqueue(103);
+            // queue.Enqueue(104);
+            // queue.Enqueue(105);
 
-            int data3 = queue.Dequeue();
-            int data4 = queue.Peek();
+            // int data3 = queue.Dequeue();
+            // int data4 = queue.Peek();
 
             // 왜 스택과 큐를 사용하는가? 의사소통에 큰 도움이 된다. 
             // 연결리스트, 동적 배열의 축소판이다.
+
+            // 그래프 순회
+            // 1. DFS (Depth First Search) 깊이 우선 탐색
+
+
+            // 2. BFS (Breadth First Search) 너비 우선 탐색
         }
     }
 }
