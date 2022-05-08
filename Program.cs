adb383c730d50d941c1500f87ac891d036d52d10
nlom0218 <nlom0218@naver.com>
2022-05-08 10:44:39 +0900

    BFS(너비 우선 탐색)

--- Program.cs
+++ Program.cs
@@ -3,67 +3,95 @@ using System.Collections.Generic;
 
 namespace Algorithm
 {
-    class Graph
-    {
-        int[,] adj = new int[6, 6]
-        {
-            {0, 1, 0, 1, 0, 0},
-            {1, 0, 1, 1, 0, 0},
-            {0, 1, 0, 0, 0, 0},
-            {1, 1, 0, 0, 0, 0},
-            {0, 0, 0, 0, 0, 1},
-            {0, 0, 0, 0, 1, 0},
-        };
+    // class Graph
+    // {
+    //     int[,] adj = new int[6, 6]
+    //     {
+    //         {0, 1, 0, 1, 0, 0},
+    //         {1, 0, 1, 1, 0, 0},
+    //         {0, 1, 0, 0, 0, 0},
+    //         {1, 1, 0, 0, 1, 0},
+    //         {0, 0, 0, 1, 0, 1},
+    //         {0, 0, 0, 0, 1, 0},
+    //     };
 
-        List<int>[] adj2 = new List<int>[]
-        {
-            new List<int>() {1, 3},
-            new List<int>() {0, 2, 3},
-            new List<int>() {1},
-            new List<int>() {0, 1},
-            new List<int>() {5},
-            new List<int>() {4},
-        };
+    //     List<int>[] adj2 = new List<int>[]
+    //     {
+    //         new List<int>() {1, 3},
+    //         new List<int>() {0, 2, 3},
+    //         new List<int>() {1},
+    //         new List<int>() {0, 1, 4},
+    //         new List<int>() {3, 5},
+    //         new List<int>() {4},
+    //     };
 
-        bool[] visited = new bool[6];
-        // 1) 우선 now부터 방문하고,
-        // 2) now와 연결된 정점들을 하나씩 확인해서, [아직 미발견(미방문) 상태라면] 방문한다.
-        public void DFS(int now)
-        {
-            System.Console.WriteLine(now);
-            visited[now] = true; // 1) 우선 now부터 방문하고,
+    //     bool[] visited = new bool[6];
+    //     // 1) 우선 now부터 방문하고,
+    //     // 2) now와 연결된 정점들을 하나씩 확인해서, [아직 미발견(미방문) 상태라면] 방문한다.
+    //     public void DFS(int now)
+    //     {
+    //         System.Console.WriteLine(now);
+    //         visited[now] = true; // 1) 우선 now부터 방문하고,
 
-            for (int next = 0; next < 6; next++)
-            {
-                if (adj[now, next] == 0) // 연결되어 있지 않으면 스킵
-                    continue;
-                if (visited[next]) // 이미 방문한 상태라면 스킵
-                    continue;
-                DFS(next);
-            }
-        }
+    //         for (int next = 0; next < 6; next++)
+    //         {
+    //             if (adj[now, next] == 0) // 연결되어 있지 않으면 스킵
+    //                 continue;
+    //             if (visited[next]) // 이미 방문한 상태라면 스킵
+    //                 continue;
+    //             DFS(next);
+    //         }
+    //     }
 
-        public void DFS2(int now)
-        {
-            System.Console.WriteLine(now);
-            visited[now] = true; // 1) 우선 now부터 방문하고,
+    //     public void DFS2(int now)
+    //     {
+    //         System.Console.WriteLine(now);
+    //         visited[now] = true; // 1) 우선 now부터 방문하고,
 
-            foreach (var next in adj2[now])
-            {
-                if (visited[next])
-                    continue;
-                DFS2(next);
-            }
-        }
+    //         foreach (var next in adj2[now])
+    //         {
+    //             if (visited[next])
+    //                 continue;
+    //             DFS2(next);
+    //         }
+    //     }
 
-        public void SearchAll()
-        {
-            visited = new bool[6];
-            for (int now = 0; now < 6; now++)
-                if (visited[now] == false)
-                    DFS(now);
-        }
-    }
+    //     public void SearchAll()
+    //     {
+    //         visited = new bool[6];
+    //         for (int now = 0; now < 6; now++)
+    //             if (visited[now] == false)
+    //                 DFS(now);
+    //     }
+
+    //     public void BFS(int start)
+    //     {
+    //         bool[] found = new bool[6];
+
+    //         Queue<int> q = new Queue<int>(); // 예약 시스템
+    //         q.Enqueue(start);
+    //         found[start] = true;
+
+    //         while (q.Count > 0)
+    //         {
+    //             int now = q.Dequeue();
+    //             System.Console.WriteLine(now);
+
+    //             for (int next = 0; next < 6; next++)
+    //             {
+    //                 if (adj[now, next] == 0) // 인접하지 않았으면 스킵
+    //                     continue;
+    //                 if (found[next]) // 이미 발견했으면 스킵
+    //                     continue;
+    //                 q.Enqueue(next);
+    //                 found[next] = true;
+
+    //             }
+    //         }
+    //     }
+    // }
+
+
     class Program
     {
         static void Main(string[] args)
@@ -126,8 +154,8 @@ namespace Algorithm
 
             // 그래프 순회
             // 1. DFS (Depth First Search) 깊이 우선 탐색
-            Graph graph = new Graph();
-            graph.SearchAll();
+            // Graph graph = new Graph();
+            // graph.BFS(0);
 
             // 2. BFS (Breadth First Search) 너비 우선 탐색
         }
