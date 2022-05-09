998176ad354dbde7bdc0aa99d7d84ca85011716c
nlom0218 <nlom0218@naver.com>
2022-05-09 23:52:03 +0900

    A* 구현 Part2

--- Player.cs
+++ Player.cs
@@ -35,7 +35,7 @@ namespace Algorithm
             PosX = posX;
             _board = board;
 
-            BFS();
+            AStar();
 
         }
 
@@ -59,6 +59,9 @@ namespace Algorithm
 
         void AStar()
         {
+            int[] deltaY = new int[] { -1, 0, 1, 0 };
+            int[] deltaX = new int[] { 0, -1, 0, 1 };
+            int[] cost = new int[] { 1, 1, 1, 1 };
             // 점수 매기기
             // F = G + H
             // F = 최종 점수 (작을 수록 좋은, 경로에 따라 달라짐)
@@ -76,16 +79,63 @@ namespace Algorithm
                 for (int x = 0; x < _board.Size; x++)
                     open[y, x] = Int32.MaxValue;
 
+            Pos[,] parent = new Pos[_board.Size, _board.Size];
+
             // 오픈리스트에 있는 정보들 중에서, 가장 좋은 후보를 빠르게 뽑아오기 위한 도구
             PriorityQueue<PQNode> pq = new PriorityQueue<PQNode>();
 
             // 시작점 발견(예약 진행)
             open[PosY, PosX] = Math.Abs(_board.DestY - PosY) + Math.Abs(_board.DestX - PosX);
             pq.Push(new PQNode() { F = Math.Abs(_board.DestY - PosY) + Math.Abs(_board.DestX - PosX), G = 0, Y = PosY, X = PosX });
+            parent[PosY, PosX] = new Pos(PosY, PosX);
 
-            while (true)
+            while (pq.Count > 0)
             {
+                // 제일 좋은 후보를 찾는다.
+                PQNode node = pq.Pop();
 
+                // 동일한 좌표를 여러 경로로 찾아서, 더 빠른 경로로 인해서 이미 방문(closed)된 경우 스킵
+                if (closed[node.Y, node.X])
+                    continue;
+
+                // 방문한다
+                closed[node.Y, node.X] = true;
+
+                // 목적지 도착했으면 바로 종료
+                if (node.Y == _board.DestY && node.X == _board.DestX)
+                    break;
+
+                // 상하좌우 등 이동할 수 있는 좌표인지 확ㅇ니해서 예약 한다.
+                for (int i = 0; i < deltaY.Length; i++)
+                {
+                    int nextY = node.Y + deltaY[i];
+                    int nextX = node.X + deltaX[i];
+
+                    // 유효 범위를 벗어났으면 스킵
+                    if (nextX < 0 || nextX >= _board.Size || nextY < 0 || nextY >= _board.Size)
+                        continue;
+                    // 벽으로 막혀서 갈 수 없으면 스킵
+                    if (_board.Tile[nextY, nextX] == Board.TileType.Wall)
+                        continue;
+                    // 이미 방문한 곳이면 스킵
+                    if (closed[nextY, nextX])
+                        continue;
+
+                    // 비용계산
+                    int g = node.G + cost[i];
+                    int h = Math.Abs(_board.DestY - nextY) + Math.Abs(_board.DestX - nextX);
+
+                    // 다른 경로에서 더 빠른 길을 이미 찾았으면 스킵
+                    if (open[nextY, nextX] < g + h)
+                        continue;
+
+                    // 얘약 진행
+                    open[nextY, nextX] = g + h;
+                    pq.Push(new PQNode() { F = g + h, G = g, Y = nextY, X = nextX });
+                    parent[nextY, nextX] = new Pos(node.Y, node.X);
+
+                    CalePathFromParent(parent);
+                }
             }
         }
 
@@ -124,6 +174,11 @@ namespace Algorithm
                 }
             }
 
+            CalePathFromParent(parent);
+        }
+
+        void CalePathFromParent(Pos[,] parent)
+        {
             int y = _board.DestY;
             int x = _board.DestX;
             while (parent[y, x].Y != y || parent[y, x].X != x)
